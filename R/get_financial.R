eaf57f716fdb366eb4ecca3df23176447e8754e3
Gooberpatrol66 <5498361+Gooberpatrol66@users.noreply.github.com>
2020-10-06 19:26:37 -0500

    Change GetURL to use edgarWebR

--- R/get_financial.R
+++ R/get_financial.R
@@ -16,19 +16,11 @@ GetFinancial <- function(statement.type, symbol, year) {
      ##   Function to acquire Instance Document URL
      GetURL <- function(symbol, year) {
           
-          lower.symbol <- tolower(symbol)
+          library(edgarWebR)
+          FilingsonEdgar <- edgarWebR::company_filings(x = symbol, type = "10-K")
+          DocumentsonEdgar <-  edgarWebR::filing_documents(x = FilingsonEdgar$href[1])
+          inst.url <- DocumentsonEdgar[DocumentsonEdgar[5] == 'XML', 4]
           
-          accession.no.raw <- GetAccessionNo(symbol, year, foreign = FALSE)
-          accession.no <- gsub("-", "" , accession.no.raw)
-          
-          CIK <- CompanyInfo(symbol)
-          CIK <- as.numeric(CIK$CIK)
-          
-          report.period <- ReportPeriod(symbol, CIK, accession.no, accession.no.raw)
-          report.period <- gsub("-", "" , report.period)
-          
-          inst.url <- paste0("https://www.sec.gov/Archives/edgar/data/", CIK, "/", 
-                             accession.no, "/", lower.symbol, "-", report.period, ".xml")
           return(inst.url)
      }
      
